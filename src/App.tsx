dd643b986bf3e4ecc0aa0fd8a2bb2fa7189b5db2
Saulo Regis de Medeiros Costa <l4910@liga.org.br>
2025-04-01 07:23:15 -0300

    Criação de componente de input

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import styles from "./app.module.css"
+import { Input } from "./components/Inputs"
 import { Header } from "./components/Header"
 import { Tip } from "./components/Tip"
 import { Letter } from "./components/Letters"
@@ -15,7 +16,19 @@ export default function App() {
       <main>
         <Header current={6} max={10} onRestart={HandlerRestartGame}/>
         <Tip tip="Linguagem de programação dinâmica" />
-        <Letter value="R"/>
+        <div className={styles.word}>
+          <Letter value="R"/>
+          <Letter value="E"/>
+          <Letter value="A"/>
+          <Letter value="C"/>
+          <Letter value="T"/>
+        </div>
+
+        <h4>Palpite</h4>
+        
+        <div>
+          <Input autoFocus maxLength={1} placeholder="?"/>
+        </div>
       </main>
     </div>
   )
