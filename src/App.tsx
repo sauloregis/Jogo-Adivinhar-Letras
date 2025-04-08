a53ec931a2d5c2f3031c9cc00e07049b2d858cac
Saulo Regis de Medeiros Costa <l4910@liga.org.br>
2025-04-08 08:07:37 -0300

    Inicio das iterações das palavras do jogo

--- src/App.tsx
+++ src/App.tsx
@@ -5,25 +5,48 @@ import { Header } from "./components/Header"
 import { Tip } from "./components/Tip"
 import { Letter } from "./components/Letter"
 import { LettersUsed } from "./components/LettersUsed"
+import { WORDS, Challenge } from "./utils/words"
+import { useState, useEffect } from "react"
 
 export default function App() {
+  const [letter, setLetter] = useState("")
+  const [attempts, setAttempts] = useState(0)
+  const [challenge, setChallenge] = useState<Challenge | null>(null)
+
 
   function HandlerRestartGame() {
-    return(
       alert("Reiniciou o jogo!")
-    )
   }
+
+  function startGame() {
+    const index = Math.floor(Math.random() * WORDS.length)
+    const randomWord = WORDS[index]
+
+    setChallenge(randomWord)
+
+    setAttempts(0)
+    setLetter("")
+  }
+
+  useEffect(() => {
+    startGame()
+  }, []) 
+
+  if(!challenge){
+    return 
+  }
+  
   return (
     <div className={styles.container}>
       <main>
-        <Header current={6} max={10} onRestart={HandlerRestartGame}/>
-        <Tip tip="Linguagem de programação dinâmica" />
+        <Header current={attempts} max={10} onRestart={HandlerRestartGame}/>
+        <Tip tip="" />
         <div className={styles.word}>
-          <Letter value="R"/>
-          <Letter value="E"/>
-          <Letter value="A"/>
-          <Letter value="C"/>
-          <Letter value="T"/>
+          {challenge.word.split("").map(() => (  //o map itera entre cada elemento da array e produz uma Letter pra cada uma
+            <Letter value={letter} />
+          ))
+
+          }
         </div>
 
         <h4>Palpite</h4>
